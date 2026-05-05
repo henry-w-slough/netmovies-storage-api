34c7e1693d87624c606c9d134de186a9657ba5f8
henry-w-slough <henry.w.slough@gmail.com>
2026-05-05 09:54:24 -0400

    Added uvicorn and FastAPI for http request handling

--- MovieParser/MovieParser.py
+++ MovieParser/MovieParser.py
@@ -1,4 +1,8 @@
 import os
+import ffmpeg
+import hashlib
+
+from .. import config
 
 
 class MovieParser:
@@ -13,13 +17,32 @@ class MovieParser:
         """
 
         self.root_dir = root_dir    
-
+        #creating root directory
         os.makedirs(self.root_dir, exist_ok=True)
 
 
     def get_movie_directory(self, movie_name:str) -> str:
         """Returns the directory of the movie given relative to the root directory of the MovieParser."""
         return os.path.join(self.root_dir, movie_name)
+    
+
+    def parse_movie(self, movie_file:str) -> None:
+        os.makedirs(self.get_movie_directory(movie_file))
+
+
+    def get_chunk_id(self, data:bytes, length:int=8) -> str:
+        """Returns a hash id based on the data and length provided."""
+        return hashlib.sha256(data).hexdigest()[:length]
+
+
+    def data_to_chunks(self, data:str, http_request:str, id_length:int=8) -> None:
+        """Takes a file and breaks it into data chunks which are placed in a given directory. Chunks are named with hash convention."""
+
+        #making the subdirectory for the movie relative to the root directory of the Parser
+        movie_dir = os.path.join(self.root_dir, http_request.name)
+        os.makedirs(movie_dir, exist_ok=True)
+
+
         
 
 
--- main.py
+++ main.py
@@ -1,4 +1,16 @@
-from MovieParser import MovieParser
+from fastapi import FastAPI
+
+app = FastAPI(root_path="/app/")
 
 
-parser = MovieParser.MovieParser("Movies")
+@app.get("/asdd/")
+def read_root():
+    return {"message": "Hello, FastAPI!"}
+
+
+@app.post("/dasd/")
+def greet_user(name: str):
+    return {"message": "Hello, " + name + "!"}
+
+
+
